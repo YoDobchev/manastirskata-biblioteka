6597f0c310cf0153aa82dbb3915e8c412daad594
Nikola Yordanov <nikolayordanov2667@gmail.com>
2023-03-31 22:26:12 +0300

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
-// router.get("/login", (req, res) => {
-//   console.log("kek");
-//   res.render("login.ejs");
-// });
-router.get("/login", (req, res) => {
-  res.render("home.ejs");
+router.get("/", (req, res) => {
+  console.log("kek");
+  res.render("login.ejs");
 });
-router.post("/login", (req, res) => {
+
+router.post("/", (req, res) => {
   // check username and password
   if (username === "myuser" && password === "mypassword") {
     req.session.loggedIn = true;
