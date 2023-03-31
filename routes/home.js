c6951ba37f54bf8a18deef628d202d016d674f77
y.dobchev <y.dobchev@myxrobotics.com>
2023-04-01 01:43:10 +0300

    nav-endpoints

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("home.ejs");
+router.get("/home", (req, res) => {
+  res.render("home.ejs", {activePage: 'home'});
+});
+
+router.get("/map", (req, res) => {
+  res.render("home.ejs", {activePage: 'map'});
+});
+
+router.get("/shop", (req, res) => {
+  res.render("home.ejs", {activePage: 'shop'});
+});
+
+router.get("/profile", (req, res) => {
+  res.render("home.ejs", {activePage: 'profile'});
 });
 
 module.exports = router;
