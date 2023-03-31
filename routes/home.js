169f9310e80c3d90865db692e43f81d158828a23
y.dobchev <y.dobchev@myxrobotics.com>
2023-04-01 00:41:07 +0300

    nav-bar-endpoints

--- routes/home.js
+++ routes/home.js
@@ -8,8 +8,20 @@ const isLoggedIn = (req, res, next) => {
     res.redirect("/login");
   }
 };
-router.get("/", isLoggedIn, (req, res) => {
-  res.render("home.ejs");
+router.get("/home", isLoggedIn, (req, res) => {
+  res.render("home.ejs", {activePage: 'home'});
+});
+
+router.get("/map", isLoggedIn, (req, res) => {
+  res.render("home.ejs", {activePage: 'map'});
+});
+
+router.get("/shop", isLoggedIn, (req, res) => {
+  res.render("home.ejs", {activePage: 'shop'});
+});
+
+router.get("/profile", isLoggedIn, (req, res) => {
+  res.render("home.ejs", {activePage: 'profile'});
 });
 
 module.exports = router;
