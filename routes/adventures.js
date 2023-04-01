2346e294eac3201bd629e1f0d1030314caac45ac
Nikola Yordanov <nikolayordanov2667@gmail.com>
2023-04-01 11:16:20 +0300

    Add files via upload

--- routes/adventures.js
+++ routes/adventures.js
@@ -4,8 +4,12 @@ var bodyParser = require("body-parser");
 const db = require("../db.js");
 var jsonParser = bodyParser.json();
 
+// Parse URL-encoded bodies (for form data)
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// Handle GET request for joining an adventure
 router.get("/joinAdventure", (req, res) => {
+  // Update the user's current adventure in the database
   db.users.update(
     { username: req.session.user },
     {
@@ -18,6 +22,7 @@ router.get("/joinAdventure", (req, res) => {
       },
     },
     (err) => {
+      // Send appropriate response based on success or failure
       if (err) {
         res.send(404);
       } else {
@@ -25,19 +30,23 @@ router.get("/joinAdventure", (req, res) => {
       }
     }
   );
+  // Find the updated user document and log it to the console
   db.findOne({ username: req.session.user }, (err, docs) => {
     console.log(docs);
   });
 });
 
+// Handle GET request for retrieving nearby adventures
 router.get("/getNearbyAdventures", urlencodedParser, (req, res) => {
+  // Get the user's location from the database
   db.users.findOne({ username: req.session.user }, (err, docs) => {
     userLocation = docs.location;
     console.log(userLocation);
-    const threshold = 0.05;
-    //about 5-6 kilometers
+    const threshold = 0.05; // about 5-6 kilometers
 
+    // Find all adventures in the database
     db.adventures.find({}, (err, docs) => {
+      // Filter adventures based on proximity to user
       const filteredDocs = docs.filter((doc) => {
         const latDiff = Math.abs(
           doc.Startlocation.coordinates[0] - userLocation.latitude
@@ -48,12 +57,16 @@ router.get("/getNearbyAdventures", urlencodedParser, (req, res) => {
         console.log(latDiff, lonDiff);
         return latDiff <= threshold && lonDiff <= threshold;
       });
+      // Send the filtered list of adventures as a JSON response
       res.json(filteredDocs);
     });
-    //NEVER USE THIS IN PRODUCTION!!! MONGO HAS A WAY MORE EFFICIENT WAY TO DO THIS, BUT THIS IS JUST FOR A HACKATHON
+    // Note: This is an inefficient way to filter by proximity and should not be used in production
   });
 });
 
-router.post("createAdventure", (req, res) => {});
+// Handle POST request for creating a new adventure
+router.post("/createAdventure", (req, res) => {
+  // TODO: Implement adventure creation
+});
 
 module.exports = router;
