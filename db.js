7cb0b18a562d238d512e86b90c84cb320fc89252
HauntedMilkshake <spaszahov@gmail.com>
2023-04-01 05:56:22 +0300

    fixed icons, map router working(somewhat) Co-authored-by: YoDobchev <YoDobchev@users.noreply.github.com>

--- db.js
+++ db.js
@@ -5,5 +5,12 @@ const db = new Datastore({
   filename: path.join(__dirname, "myDb.db"),
   autoload: true,
 });
-
+db.users = new Datastore({
+  filename: path.join(__dirname, "users.db"),
+  autoload: true,
+});
+db.adventures = new Datastore({
+  filename: path.join(__dirname, "adventures.db"),
+  autoload: true,
+});
 module.exports = db;
--- index.js
+++ index.js
@@ -4,22 +4,41 @@ const app = express();
 const session = require("express-session");
 const port = 3000;
 app.set("view engine", "ejs");
+const db = require("./db.js");
+const oneDay = 1000 * 60 * 60 * 24;
 app.use(
   session({
-    secret: "my-secret-key", // replace with a secret key of your own
+    secret: "ijfrjeogphgp4hgpo",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
     resave: false,
-    saveUninitialized: false,
   })
 );
+
 const isLoggedIn = (req, res, next) => {
-  if (req.session.loggedIn) {
+  if (req.session.ID) {
     next();
   } else {
+    // next();
+
     res.redirect("/login");
   }
 };
+app.get("/", isLoggedIn, (req, res) => {
+  res.redirect("/home");
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-app.use("/home", require("./routes/home.js"));
+app.post("/locationEvent", isLoggedIn, (req, res) => {
+  db.users.update(
+    { username: req.session.user },
+    { $set: { location: req.body } },
+    {},
+    (err) => {}
+  );
+});
 app.use("/login", require("./routes/login.js"));
+app.use("/signup", require("./routes/signup.js"));
+app.use("/", isLoggedIn, require("./routes/adventures.js"));
+app.use("/", isLoggedIn, require("./routes/home.js"));
