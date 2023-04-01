a44d18061e3861bdc858f54d14b32545b61061dc
nike <nikolayordanov2667@gmail.com>
2023-04-01 15:57:59 +0300

    opa)

--- db.js
+++ db.js
@@ -14,9 +14,16 @@ db.adventures = new Datastore({
   autoload: true,
 });
 db.adventures.insert({
+  name: `Monasite test nomer ${Math.floor(Math.random() * 20)}`,
   Startlocation: {
     type: "Point",
     coordinates: [42.0230861, 23.0854894],
   },
+  locations: [
+    { latitude: 42.0343, longitude: 23.11483 },
+    { latitude: 42.0267, longitude: 23.0971 },
+    { latitude: 42.0227, longitude: 23.0903 },
+    { latitude: 42.0312, longitude: 23.0808 },
+  ],
 });
 module.exports = db;
