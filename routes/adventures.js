051c710b49cea3d56c8dc4fe36b6d6a7bddbed57
Nikola Yordanov <nikolayordanov2667@gmail.com>
2023-04-01 14:32:38 +0300

    Add files via upload

--- routes/adventures.js
+++ routes/adventures.js
@@ -5,29 +5,6 @@ const db = require("../db.js");
 var jsonParser = bodyParser.json();
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-router.get("/getNearbyAdventures", urlencodedParser, (req, res) => {
-  db.users.findOne({ username: req.session.user }, (err, docs) => {
-    userLocation = docs.location;
-    console.log(userLocation);
-    const threshold = 0.05;
-    //about 5-6 kilometers
-
-    db.adventures.find({}, (err, docs) => {
-      const filteredDocs = docs.filter((doc) => {
-        const latDiff = Math.abs(
-          doc.Startlocation.coordinates[0] - userLocation.latitude
-        );
-        const lonDiff = Math.abs(
-          doc.Startlocation.coordinates[1] - userLocation.longitude
-        );
-        console.log(latDiff, lonDiff);
-        return latDiff <= threshold && lonDiff <= threshold;
-      });
-      res.json(filteredDocs);
-    });
-    //NEVER USE THIS IN PRODUCTION!!! MONGO HAS A WAY MORE EFFICIENT WAY TO DO THIS, BUT THIS IS JUST FOR A HACKATHON
-  });
-});
 
 router.post("createAdventure", (req, res) => {});
 
--- routes/home.js
+++ routes/home.js
@@ -1,20 +1,56 @@
 const express = require("express");
 const router = express.Router();
+const bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const db = require("../db");
+
+router.use(urlencodedParser, (req, res, next) => {
+  db.users.findOne({ username: req.session.user }, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.sendStatus(500);
+    }
+    const userLocation = docs.location;
+    console.log(userLocation);
+    const threshold = 0.5;
+    //about 5-6 kilometers
+
+    db.adventures.find({}, (err, docs) => {
+      if (err) {
+        console.log(err);
+        return res.sendStatus(500);
+      }
+      const filteredDocs = docs.filter((doc) => {
+        const latDiff = Math.abs(
+          doc.Startlocation.coordinates[0] - userLocation.latitude
+        );
+        const lonDiff = Math.abs(
+          doc.Startlocation.coordinates[1] - userLocation.longitude
+        );
+        console.log(latDiff, lonDiff);
+        return latDiff <= threshold && lonDiff <= threshold;
+      });
+      req.nearbyAdvs = filteredDocs;
+      console.log(filteredDocs);
+      next();
+    });
+  });
+});
 
 router.get("/home", (req, res) => {
-  res.render("home.ejs", { activePage: "home" });
+  res.render("home.ejs", { advs: req.nearbyAdvs, activePage: "home" });
 });
 
 router.get("/map", (req, res) => {
-  res.render("home.ejs", { activePage: "map" });
+  res.render("home.ejs", { advs: req.nearbyAdvs, activePage: "map" });
 });
 
 router.get("/shop", (req, res) => {
-  res.render("home.ejs", { activePage: "shop" });
+  res.render("home.ejs", { advs: req.nearbyAdvs, activePage: "shop" });
 });
 
 router.get("/profile", (req, res) => {
-  res.render("home.ejs", { activePage: "profile" });
+  res.render("home.ejs", { advs: req.nearbyAdvs, activePage: "profile" });
 });
 
 module.exports = router;
