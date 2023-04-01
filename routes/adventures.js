bbb9a6854301318d94e07f9c51d8160857d59a03
Nikola Yordanov <nikolayordanov2667@gmail.com>
2023-04-01 07:00:51 +0300

    Add files via upload

--- routes/adventures.js
+++ routes/adventures.js
@@ -4,35 +4,31 @@ var bodyParser = require("body-parser");
 const db = require("../db.js");
 var jsonParser = bodyParser.json();
 
-// create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 router.get("/getNearbyAdventures", urlencodedParser, (req, res) => {
   db.users.findOne({ username: req.session.user }, (err, docs) => {
     userLocation = docs.location;
     console.log(userLocation);
-    db.adventures.find(
-      {
-        Startlocation: {
-          $near: {
-            $geometry: {
-              type: "Point",
-              coordinates: [userLocation.longitude, userLocation.latitude],
-            },
-            $maxDistance: 5000, // in meters
-          },
-        },
-      },
-      (err, nearbyAdventures) => {
-        if (err) {
-          console.log(err);
-          res.status(200).send("Error finding nearby adventures");
-        } else {
-          res.send(JSON.stringify(nearbyAdventures));
-        }
-      }
-    );
+    const threshold = 0.05;
+    //about 5-6 kilometers
+
+    db.adventures.find({}, (err, docs) => {
+      const filteredDocs = docs.filter((doc) => {
+        const latDiff = Math.abs(
+          doc.Startlocation.coordinates[0] - userLocation.latitude
+        );
+        const lonDiff = Math.abs(
+          doc.Startlocation.coordinates[1] - userLocation.longitude
+        );
+        console.log(latDiff, lonDiff);
+        return latDiff <= threshold && lonDiff <= threshold;
+      });
+      res.json(filteredDocs);
+    });
+    //NEVER USE THIS IN PRODUCTION!!! MONGO HAS A WAY MORE EFFICIENT WAY TO DO THIS, BUT THIS IS JUST FOR A HACKATHON
   });
 });
+
 router.post("createAdventure", (req, res) => {});
 
 module.exports = router;
