725e4f2d156862649042b3c4d7c2d3ff8d0f5557
nike <nikolayordanov2667@gmail.com>
2023-04-01 03:13:53 +0300

    added cool endpoints

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const session = require("express-session");
 const port = 3000;
 app.set("view engine", "ejs");
+const db = require("./db.js");
 const oneDay = 1000 * 60 * 60 * 24;
 app.use(
   session({
@@ -29,7 +30,15 @@ app.get("/", isLoggedIn, (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+app.post("/locationEvent", isLoggedIn, (req, res) => {
+  db.users.update(
+    { username: req.session.user },
+    { $set: { location: req.body } },
+    {},
+    (err) => {}
+  );
+});
 app.use("/login", require("./routes/login.js"));
 app.use("/signup", require("./routes/signup.js"));
-app.use("/adventures", isLoggedIn, require("./routes/signup.js"));
+app.use("/", isLoggedIn, require("./routes/adventures.js"));
 app.use("/", isLoggedIn, require("./routes/home.js"));
