498f42206eaf3a9247d03912e1e5d28e02de5a9b
HauntedMilkshake <spaszahov@gmail.com>
2023-04-01 03:12:09 +0300

    test commit

--- routes/home.js
+++ routes/home.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const app = exoress();
 const router = express.Router();
 const isLoggedIn = (req, res, next) => {
   if (req.session.loggedIn) {
@@ -11,5 +12,8 @@ const isLoggedIn = (req, res, next) => {
 router.get("/", isLoggedIn, (req, res) => {
   res.render("home.ejs");
 });
+app.set('js', 'text/javascript');
 
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 module.exports = router;
