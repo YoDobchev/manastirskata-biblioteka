088e52fa9143f8e5d7823aeb575ea2c89dc908e5
nike <nikolayordanov2667@gmail.com>
2023-04-01 03:13:53 +0300

    added cool endpoints

--- routes/adventures.js
+++ routes/adventures.js
@@ -3,5 +3,30 @@ const router = express.Router();
 var bodyParser = require("body-parser");
 const db = require("../db.js");
 
-router.post("/", (req, res) => {});
+router.get("/getNearbyAdventures", (req, res) => {
+  const userLocation = req.body;
+
+  db.adventures.find(
+    {
+      Startlocation: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [userLocation.longitude, userLocation.latitude],
+          },
+          $maxDistance: 5000, // in meters
+        },
+      },
+    },
+    (err, nearbyAdventures) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Error finding nearby adventures");
+      } else {
+        res.send(nearbyAdventures);
+      }
+    }
+  );
+});
+
 module.exports = router;
--- routes/home.js
+++ routes/home.js
@@ -2,19 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/home", (req, res) => {
-  res.render("home.ejs", {activePage: 'home'});
+  res.render("home.ejs", { activePage: "home" });
 });
 
 router.get("/map", (req, res) => {
-  res.render("home.ejs", {activePage: 'map'});
+  res.render("home.ejs", { activePage: "map" });
 });
 
 router.get("/shop", (req, res) => {
-  res.render("home.ejs", {activePage: 'shop'});
+  res.render("home.ejs", { activePage: "shop" });
 });
 
 router.get("/profile", (req, res) => {
-  res.render("home.ejs", {activePage: 'profile'});
+  res.render("home.ejs", { activePage: "profile" });
 });
 
 module.exports = router;
