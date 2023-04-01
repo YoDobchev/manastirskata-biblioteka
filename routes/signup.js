f5b19e4032a740969395be0c23ccb5cd1a45ec5b
Nikola Yordanov <nikolayordanov2667@gmail.com>
2023-04-01 09:12:25 +0300

    Add files via upload

--- routes/signup.js
+++ routes/signup.js
@@ -24,6 +24,7 @@ router.post("/", (req, res) => {
           .update(req.body.password)
           .digest("hex"),
         location: { latitude: 0, longitude: 0 },
+        currentAdventure: null,
       });
     } else {
       res.redirect("/login");
