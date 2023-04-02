d2c7af9dd0dafb634bfc487c34ed8825bbbcda6f
nike <nikolayordanov2667@gmail.com>
2023-04-02 12:49:22 +0300

    document

--- routes/login.js
+++ routes/login.js
@@ -1,32 +1,50 @@
 const express = require("express");
 const router = express.Router();
-let bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const db = require("../db.js");
-let crypto = require("crypto");
+const crypto = require("crypto");
+
+// Render login page
 router.get("/", (req, res) => {
   res.render("login.ejs");
 });
 
+// Parse request body in JSON format
 router.use(bodyParser.json());
+
+// Handle login form submission
 router.post("/", (req, res) => {
+  // Hash the password using SHA256 algorithm
   const hashedPassword = crypto
     .createHash("sha256")
     .update(req.body.password)
     .digest("hex");
+
+  // Set the username in the session
   req.session.user = req.body.username;
 
-  // check username and password
+  // Check if the username exists in the database
   db.users.findOne({ username: req.body.username }, (err, docs) => {
     if (!docs || err) {
-      res.send("wrong password bro:(");
+      // If the username is not found or there is an error, send an error message
+      res.send("Wrong username or password.");
     } else {
+      // If the username is found, check if the password matches the hashed password
       if (docs.password == hashedPassword) {
-        req.session.ID = crypto
+        // If the password matches, generate a session ID using a combination of the password and a salt
+        const sessionID = crypto
           .createHash("sha256")
-          .update(req.body.password + req.body.password)
+          .update(req.body.password + "salt")
           .digest("hex");
 
+        // Set the session ID in the session
+        req.session.ID = sessionID;
+
+        // Redirect to the home page
         res.redirect("/home");
+      } else {
+        // If the password does not match, send an error message
+        res.send("Wrong username or password.");
       }
     }
   });
