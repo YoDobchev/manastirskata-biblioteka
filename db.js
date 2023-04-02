f424210fe1407aafee02c63c3c9040b72f7f8fa1
nike <nikolayordanov2667@gmail.com>
2023-04-02 03:52:59 +0300

    elo

--- db.js
+++ db.js
@@ -1,10 +1,7 @@
 const Datastore = require("nedb");
 const path = require("path");
 
-const db = new Datastore({
-  filename: path.join(__dirname, "myDb.db"),
-  autoload: true,
-});
+const db = {};
 db.users = new Datastore({
   filename: path.join(__dirname, "users.db"),
   autoload: true,
@@ -13,19 +10,19 @@ db.adventures = new Datastore({
   filename: path.join(__dirname, "adventures.db"),
   autoload: true,
 });
-if (
-  db.adventures.findOne({}, (err, docs) => {
-    if (docs != null) {
-      db.adventures.insert({
-        name: `Monasite test nomer ${Math.floor(Math.random() * 20)}`,
-        locations: [
-          { latitude: 42.0343, longitude: 23.11483 },
-          { latitude: 42.0267, longitude: 23.0971 },
-          { latitude: 42.0227, longitude: 23.0903 },
-          { latitude: 42.0312, longitude: 23.0808 },
-        ],
-      });
-    }
-  })
-)
-  module.exports = db;
+
+db.adventures.findOne({}, (err, docs) => {
+  if (docs == null) {
+    db.adventures.insert({
+      name: `Monasite test nomer ${Math.floor(Math.random() * 20)}`,
+      locations: [
+        { latitude: 42.0343, longitude: 23.11483 },
+        { latitude: 42.0267, longitude: 23.0971 },
+        { latitude: 42.0227, longitude: 23.0903 },
+        { latitude: 42.0312, longitude: 23.0808 },
+      ],
+    });
+  }
+});
+
+module.exports = db;
